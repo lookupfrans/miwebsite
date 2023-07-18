e4c0ef66703eded224a4c585bbc5f59649a5903b
Fransen Steven Roby <99979348+lookupfrans@users.noreply.github.com>
2023-07-18 20:11:52 +0700

    Update ua.php

--- ua.php
+++ ua.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Shelf store</title>
+  <title>H2O store</title>
   <link rel="stylesheet" href="responsive.css">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
   <style>
