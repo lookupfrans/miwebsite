ef06bf34a7251bbeb4c71a034ff5236076f5c189
Fransen Steven Roby <99979348+lookupfrans@users.noreply.github.com>
2023-07-27 21:15:14 +0700

    Add files via upload

---
+++ wa.js
@@ -0,0 +1,5 @@
+document.getElementById("whatsapp-button").addEventListener("click", function() {
+    var phoneNumber = "6287798961630";
+    var url = "https://wa.me/" + phoneNumber;
+    window.open(url, "_blank");
+});
---
+++ wa.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>H2O store</title>
+    <style>
+
+.whatsapp-widget {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  z-index: 9999;
+}
+
+#whatsapp-button {
+    font-family: 'Amatic SC', cursive;
+  display: inline-block;
+  padding: 10px 20px;
+  background-color: #000;
+  color: #fff;
+  text-decoration: none;
+  border-radius: 5px;
+}
+        </style>
+</head>
+<body>
+    <div class="whatsapp-widget">
+        <a id="whatsapp-button" href="#">Chat on WhatsApp</a>
+    </div>
+
+    <script src="js/wa.js"></script>
+</body>
+</html>
