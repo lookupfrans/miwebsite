e63a409abefd57771c4518e0bbfe0f9a380b294e
Fransen Steven Roby <99979348+lookupfrans@users.noreply.github.com>
2023-08-03 08:08:21 +0700

    Update pp.php

--- pp.php
+++ pp.php
@@ -14,7 +14,7 @@ include('header.php');
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>H2O store</title>
   <link rel="stylesheet" href="responsive.css">
-  <link rel="icon" type="image/x-icon" href="favicon.ico">
+  <link rel="icon" type="image/png" sizes="180x180" href="fav.png">
 </head>
 <style>
 
