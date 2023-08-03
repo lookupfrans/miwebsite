a53e8970c34869e1ab01cbfc8001ed90c5ee251e
Fransen Steven Roby <99979348+lookupfrans@users.noreply.github.com>
2023-08-03 07:53:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <link href="https://fonts.googleapis.com/css?family=Amatic+SC&amp;display=swap" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="styles.css">
   <link rel="stylesheet" type="text/css" href="responsive.css">
-  <link rel="icon" type="image/x-icon" href="fav.ico">
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
   <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3624269353063512"
      crossorigin="anonymous"></script>
 </head>
