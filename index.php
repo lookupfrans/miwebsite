35740e04e9fbe5387b9f22b87aa3e5453498a996
Fransen Steven Roby <99979348+lookupfrans@users.noreply.github.com>
2023-08-14 21:43:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 <html lang="en">
 
 <head>
-  <title>Stevenhoven store</title>
+  <title>H2O store</title>
   <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
   <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Amatic+SC&amp;display=swap" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="styles.css">
   <link rel="stylesheet" type="text/css" href="responsive.css">
-  <link rel="icon" type="image/png" sizes="180x180" href="favo.png">
+  <link rel="icon" type="image/png" sizes="180x180" href="fav.png">
   <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3624269353063512"
      crossorigin="anonymous"></script>
 </head>
@@ -16,7 +16,7 @@
 <body oncontextmenu="return false">
 
   <div id="app" class="container">
-    <h2>Stevenhoven store</h2>
+    <h2>H2O store</h2>
     <h3>Min 100.000</h3>
     <div class="text-right"><span class="cartcount">{{cartItems.length}} item(s) di keranjang</span></div>
 
@@ -132,13 +132,13 @@
               <tr>
                 <td>
                 <td style="font-size:20px;text-align:center;"><input type="checkbox" class="myCheck" required>Dengan
-                  menekan tombol proses, anda telah paham dan setuju terhadap <a href="pp.php"> Ketentuan dan Privasi Stevenhoven
+                  menekan tombol proses, anda telah paham dan setuju terhadap <a href="pp.php"> Ketentuan dan Privasi H2O
                     store</a></td>
                 </td>
               </tr>
             </table>
           </form><br/>
-          <h6 style="text-align:right;font-size:20px;font-weight:bold;color:#fff;">&#169; Stevenhoven store</h6>
+          <h6 style="text-align:right;font-size:20px;font-weight:bold;color:#fff;">&#169; H2O store</h6>
         </div>
 
         <div id="message"></div>
