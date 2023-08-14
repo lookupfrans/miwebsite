141330a02dfd936936856c2aae99e8e0a89f10a9
Fransen Steven Roby <99979348+lookupfrans@users.noreply.github.com>
2023-08-14 21:16:06 +0700

    Update pp.php

--- pp.php
+++ pp.php
@@ -14,7 +14,7 @@ include('header.php');
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Stevenhoven store</title>
   <link rel="stylesheet" href="responsive.css">
-  <link rel="icon" type="image/png" sizes="180x180" href="fav.png">
+  <link rel="icon" type="image/png" sizes="180x180" href="favo.png">
 </head>
 <style>
 
