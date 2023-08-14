cafcf062a469787f27bddc3c0de0f2a5a22647ac
Fransen Steven Roby <99979348+lookupfrans@users.noreply.github.com>
2023-08-14 21:13:35 +0700

    Update pp.php

--- pp.php
+++ pp.php
@@ -12,7 +12,7 @@ include('header.php');
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>H2O store</title>
+  <title>Stevenhoven store</title>
   <link rel="stylesheet" href="responsive.css">
   <link rel="icon" type="image/png" sizes="180x180" href="fav.png">
 </head>
