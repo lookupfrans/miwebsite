cef812365cd2fd8799265845e24f0872fd0fa0b4
Fransen Steven Roby <99979348+lookupfrans@users.noreply.github.com>
2023-08-14 21:45:25 +0700

    Update wa.php

--- wa.php
+++ wa.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Stevenhoven store</title>
+    <title>H2O store</title>
     <link rel="stylesheet" type="text/css" href="responsive.css">
     <style>
         .whatsapp-widget {
