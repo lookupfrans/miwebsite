5a1e838d68cf2dc33ae92d4fe3527a34decbd2b5
Fransen Steven Roby <99979348+lookupfrans@users.noreply.github.com>
2023-09-26 13:01:42 +0700

    Update ua.php

--- ua.php
+++ ua.php
@@ -7,7 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Stevenhoven Store</title>
   <link rel="stylesheet" href="responsive.css">
-  <link rel="icon" type="image/png" sizes="180x180" href="fav.png">
+  <link rel="icon" type="image/png" sizes="180x180" href="favo.png">
   <style>
      .heading {
             margin-left: 250px;
