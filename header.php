411b27763f2cf1dcef7f538697abe54a537a328b
Fransen Steven Roby <99979348+lookupfrans@users.noreply.github.com>
2023-09-26 12:59:03 +0700

    Update header.php

--- header.php
+++ header.php
@@ -4,10 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>H2O store</title>
+    <title>Stevenhoveb Store</title>
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Amatic+SC&amp;display=swap" rel="stylesheet">
-    <link rel="icon" type="image/png" sizes="180x180" href="fav.png">
+    <link rel="icon" type="image/png" sizes="180x180" href="favo.png">
     <style>
         h2 {
             font-size: 30px;
