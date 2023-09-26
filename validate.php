33e82dc06778499058a7d793492b276d95ab3081
Fransen Steven Roby <99979348+lookupfrans@users.noreply.github.com>
2023-09-26 13:14:21 +0700

    Update validate.php

--- validate.php
+++ validate.php
@@ -8,7 +8,7 @@ include('header.php);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>H2O store</title>
+    <title>Stevenhoven Store</title>
     <link rel="stylesheet" href="responsive.css">
     <link rel="icon" type="image/png" sizes="180x180" href="favo.png">
     <style>
