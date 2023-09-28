bd4f12a3c0f313f5b92a7d869a917edb7dd530bc
Fransen Steven Roby <99979348+lookupfrans@users.noreply.github.com>
2023-09-28 13:23:37 +0700

    Update ua.php

--- ua.php
+++ ua.php
@@ -100,8 +100,6 @@ include('header.php');
             merely be <mark style="color:yellow">responsible</mark> for those such items which we have mailed and yet
             have not been notified of their undelivery within this time frame.</p>
 
-          <div class="message-wrapper" style="padding:0px;border-top:1px solid #fff;">
-            <div class="container">
           <h1 style="margin-top: 100px; text-align: center; font-family: 'Cormorant',; padding-top: 0px; opacity: 0.5;">
             Privacy Policy</h1>
           <hr>
@@ -150,8 +148,8 @@ include('header.php');
             hanya <mark style="color: yellow">bertanggung jawab</mark> atas barang-barang yang belum kami kirimkan
             belum diberitahu tentang kegagalan pengirimannya dalam jangka waktu ini.</p>
 
-          <div class="message-wrapper" style="padding:0px;border-top:1px solid #fff;">
-            <div class="container">
+        <div style="border:1px solid #fff;">
+        </div>
               <h1
                 style="margin-top: 100px; text-align: center; font-family: 'Cormorant',; padding-top: 0px; opacity: 0.5;">
                 Kebijakan Privasi</h1>
