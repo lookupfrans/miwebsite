983fabc6ed1da4169dcc4fa51f88b08a63a16b49
Fransen Steven Roby <99979348+lookupfrans@users.noreply.github.com>
2023-09-28 13:49:29 +0700

    Update header.php

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
     <title>Stevenhoven store</title>
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Amatic+SC&amp;display=swap" rel="stylesheet">
+    <link rel="icon" type="image/png" sizes="180x180" href="favo.png">
     <style>
         h2 {
             font-size: 30px;
