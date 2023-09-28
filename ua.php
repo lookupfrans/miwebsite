b8ae9ca4b34c943a7ed48bca6b7c2d111bc106a6
Fransen Steven Roby <99979348+lookupfrans@users.noreply.github.com>
2023-09-28 12:56:07 +0700

    Update ua.php

--- ua.php
+++ ua.php
@@ -1,3 +1,7 @@
+<?php
+include('header.php');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,56 +13,175 @@
   <link rel="stylesheet" href="responsive.css">
   <link rel="icon" type="image/png" sizes="180x180" href="favo.png">
   <style>
-     .heading {
-            margin-left: 250px;
-            margin-top: 10%;
-        }
+    .heading {
+      margin-left: 250px;
+      margin-top: 10%;
+    }
 
-        .heading1 {
-            margin-left: 250px;
-        }
-        
-        .heading1 a {
-            text-decoration: none;
-            color: red;
-        }
+    .heading1 {
+      margin-left: 250px;
+    }
 
-        .heading1 a:hover {
-            border-bottom: 1px solid;
-        }
+    .heading1 a {
+      text-decoration: none;
+      color: red;
+    }
+
+    .heading1 a:hover {
+      border-bottom: 1px solid;
+    }
+
+    .ref {
+      color: red;
+      text-decoration: none;
+    }
+
+    .ref:hover {
+      border-bottom: 1px solid;
+    }
+
+    header {
+      color: #fff;
+      padding: 10px;
+      float: right;
+    }
+
+    nav ul {
+      list-style-type: none;
+      padding: 0;
+    }
+
+    nav li {
+      display: inline;
+      margin-right: 10px;
+    }
+
+    nav a {
+      text-decoration: none;
+      color: #fff;
+      
+      margin: 0 5px 0 5px;
+    }
   </style>
-</head>
 
 <body oncontextmenu="return false">
-<div class="message-wrapper" style="padding-right:200px;padding-left:200px;margin-left:0px;">
-  <div class="container">
-    <h1 style="margin-top: 0px; text-align: center; font-family: 'Cormorant',; padding-top: 0px; opacity: 0.5;">
-      User Agreement</h1>
-    <hr>
-    <b>DISCLAIMER</b>
-    <p style="margin-top: 0px;text-align: justify; padding-bottom: 50px;padding-top:10px;">
-      Please be <mark style="color:yellow">aware</mark> that our products’ colours may look slightly different to
-      how they appear on devices― and colours may vary from one batch to the next. Due to the nature
-      of the living colours of our natural dyes, colours may change and mature over time.
-      Once our orders for meterage of cloth have been cut from the roll, we are no longer able to offer <mark
-        style="color:yellow">returns</mark> or <mark style="color:yellow">refunds</mark>.</p>
-    <b>UNDELIVERED ITEMS</b>
 
-    <p style="padding-bottom: 40px; text-align: justify; margin-top: 10px;">For posted items which are undelivered,
-      we request notification within 2 weeks, so that the item may be tracked and recovered. Any posted items which
-      remain undelivered
-      and unnotified to us, may be destroyed by the postal service within 1-2 months on non-delivery. We cannot
-      merely be <mark style="color:yellow">responsible</mark> for those such items which we have mailed and yet
-      have not been notified of their undelivery within this time frame.</p>
-  </div>
+  <div class="message-wrapper" style="padding-right:200px;padding-left:200px;margin-left:0px;">
+    <div class="container">
+      <div id="content">
+        <div id="agreement-en">
+          <h1 style="margin-top: 0px; text-align: center; font-family: 'Cormorant',; padding-top: 0px; opacity: 0.5;">
+            User Agreement</h1>
+          <hr>
+          <b>DISCLAIMER</b>
+          <p style="margin-top: 0px; padding-bottom: 50px;padding-top:10px;">
+            Please be <mark style="color:yellow">aware</mark> that our products’ colours may look slightly different to
+            how they appear on devices― and colours may vary from one batch to the next. Due to the nature
+            of the living colours of our natural dyes, colours may change and mature over time.
+            Once our orders for meterage of cloth have been cut from the roll, we are no longer able to offer <mark
+              style="color:yellow">returns</mark> or <mark style="color:yellow">refunds</mark>.</p>
 
-   <?php
-    include('wa.php');
+          <b>UNDELIVERED ITEMS</b>
+          <p style="padding-bottom: 40px; text-align: justify; margin-top: 10px;">For posted items which are
+            undelivered,
+            we request notification within 2 weeks, so that the item may be tracked and recovered. Any posted items
+            which
+            remain undelivered
+            and unnotified to us, may be destroyed by the postal service within 1-2 months on non-delivery. We cannot
+            merely be <mark style="color:yellow">responsible</mark> for those such items which we have mailed and yet
+            have not been notified of their undelivery within this time frame.</p>
 
-  ?>
+          <h1 style="margin-top: 100px; text-align: center; font-family: 'Cormorant',; padding-top: 0px; opacity: 0.5;">
+            Privacy Policy</h1>
+          <hr>
+          <b style="padding-left:0px;">INFORMATION</b>
+          <p style="margin-top: 5px; text-align: justify; margin-left:0px;;padding-top:5px;">We record names and
+            contact email
+            addresses via the email sign-up box and pop-up boxes on our website.
+            We use this information to send out occasional emails to update our followers with newsletters (blog
+            articles,
+            announcements, reports), promotional (sales, deals, gifts, offers), partnerships and funding cooperation
+            (business deals, partnership opportunities, investment), product/service (updates, new or removed) or
+            market
+            research (surveys, qual & quant research, feedback, ratings and reviews).
+            Don't be worry, we collect and securely store this informaation. We do not share this information with
+            anyone else. Despite, it may be accessed by staff or assistants who help with our social media
+            marketing,
+            but we do not allow anyone to collect or store this information. This data is stored with us
+            indefinitely,
+            or until the client may wish to remove
+            their contact information from our mailing list, at which point it is deleted from our files.
+            If you wish to unsubscribe from our mailing list you are welcome to do this at any time.</p>
+        </div>
+      </div>
 
-  <br /><br />
-  
-  </body>
+      <div id="content">
+        <div id="agreement-es" style="display: none;">
+          <h1 style="margin-top: 0px; text-align: center; font-family: 'Cormorant',; padding-top: 0px; opacity: 0.5;">
+            Persetujuan Pengguna</h1>
+          <hr>
+          <b>DISKLAIMER</b>
+          <p style="margin-top: 0px; padding-bawah: 50px;padding-top:10px;">
+            Harap <mark style="color: yellow">perhatikan</mark> bahwa warna produk kami mungkin terlihat sedikit berbeda
+            tampilannya di perangkat― dan warnanya mungkin berbeda dari satu batch ke batch berikutnya. Karena sifatnya
+            Dari warna-warna hidup dari pewarna alami kita, warna dapat berubah dan matang seiring berjalannya waktu.
+            Setelah pesanan kain meteran kami dipotong dari gulungan, kami tidak dapat lagi menawarkan <tanda
+              style="color:kuning">pengembalian</mark> atau <mark style="color:kuning">pengembalian dana</mark>.</p>
+
+          <b>BARANG TIDAK TERKIRIM</b>
+          <p style="padding-bottom: 40px; text-align: justify; margin-top: 10px;">Untuk item yang diposting namun tidak
+            terkirim,
+            kami meminta pemberitahuan dalam waktu 2 minggu, sehingga barang dapat dilacak dan dipulihkan. Setiap item
+            yang diposting yang
+            tetap tidak terkirim
+            dan tanpa pemberitahuan kepada kami, dapat dimusnahkan oleh layanan pos dalam waktu 1-2 bulan jika tidak
+            terkirim. Kita tidak bisa
+            hanya <mark style="color: yellow">bertanggung jawab</mark> atas barang-barang yang belum kami kirimkan
+            belum diberitahu tentang kegagalan pengirimannya dalam jangka waktu ini.</p>
+
+          <div class="message-wrapper" style="padding:0px;border-top:1px solid #fff;">
+            <div class="container">
+              <h1
+                style="margin-top: 100px; text-align: center; font-family: 'Cormorant',; padding-top: 0px; opacity: 0.5;">
+                Kebijakan Privasi</h1>
+              <hr>
+              <b style="padding-left:0px;">INFORMASI</b>
+              <p style="margin-top: 5px; text-align: justify; margin-left:0px;;padding-top:5px;">Kami mencatat nama dan
+                Kontak Email
+                alamat melalui kotak pendaftaran email dan kotak pop-up di situs web kami.
+                Kami menggunakan informasi ini untuk mengirimkan email sesekali untuk memperbarui pengikut kami dengan
+                buletin (blog
+                artikel,
+                pengumuman, laporan), promosi (penjualan, penawaran, hadiah, penawaran), kemitraan dan kerja sama
+                pendanaan
+                (kesepakatan bisnis, peluang kemitraan, investasi), produk/layanan (pembaruan, baru atau dihapus) atau
+                pasar
+                penelitian (survei, penelitian kualitas & kuantitas, umpan balik, penilaian dan ulasan).
+                Jangan khawatir, kami mengumpulkan dan menyimpan informasi ini dengan aman. Kami tidak membagikan
+                informasi ini kepada
+                orang lain. Meskipun demikian, itu dapat diakses oleh staf atau asisten yang membantu media sosial kita
+                pemasaran,
+                namun kami tidak mengizinkan siapa pun mengumpulkan atau menyimpan informasi ini. Data ini disimpan
+                bersama kami
+                tanpa batas waktu,
+                atau sampai klien mungkin ingin menghapusnya
+                informasi kontak mereka dari milis kami, yang kemudian akan dihapus dari file kami.
+                Jika Anda ingin berhenti berlangganan dari milis kami, Anda dapat melakukannya kapan saja.</p>
+            </div>
+          </div>
+        </div>
+
+
+        <br /><br />
+
+        <script src="lang.js"></script>
+
+        <?php
+        include('wa.php');
+
+        ?>
+
+        <php echo "<p style='margin-left: 390px;'>&nbsp;<a href=" index.php" class="ref">Silakan kembali</a></p>
+</body>
 
 </html>
