5d22a473999a5a619f70f84bfb43a239c22283e6
Fransen Steven Roby <99979348+lookupfrans@users.noreply.github.com>
2023-10-29 12:53:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const products = [
-  { id: 1, title: 'STARCROOS SHIRT', price: 500000, qty: 1, image: 'bg9.jpg' },
-  { id: 2, title: 'STARCROOS SHIRT', price: 500000, qty: 1, image: 'bg11.jpg' },
-  { id: 3, title: 'T-S - STARCROOS', price: 300000, qty: 1, image: 'pic16.jpg' },
-  { id: 4, title: 'T-S - STARCROOS', price: 300000, qty: 1, image: 'pic18.jpg' },
-  { id: 5, title: 'THRASHER', price: 200000, qty: 1, image: 'bg4.jpg' },
-  { id: 6, title: 'THRASHER', price: 200000, qty: 1, image: 'bg5.jpg' },
-  { id: 7, title: 'CROZZED', price: 250000, qty: 1, image: 'img1.jpg' },
+  { id: 1, title: 'SHRT - STARCROOS', price: 500000, qty: 1, image: 'bg9.jpg' },
+  { id: 2, title: 'SHRT - STARCROOS', price: 500000, qty: 1, image: 'bg11.jpg' },
+  { id: 3, title: 'TS - STARCROOS', price: 300000, qty: 1, image: 'pic16.jpg' },
+  { id: 4, title: 'TS - STARCROOS', price: 300000, qty: 1, image: 'pic18.jpg' },
+  { id: 5, title: 'TS - VICIOUS CIRCLE - GILDAN', price: 200000, qty: 1, image: 'bg4.jpg' },
+  { id: 6, title: 'TS - OBSESSED - GILDAN', price: 200000, qty: 1, image: 'bg5.jpg' },
+  { id: 7, title: 'TS - CROZZED', price: 250000, qty: 1, image: 'img1.jpg' },
   { id: 8, title: 'The OPPRESSED HOODIE', price: 648000, qty: 1, image: 'img2.jpg' },
   { id: 9, title: 'WARZONE HOODIE', price: 732000, qty: 1, image: 'img3.jpg' },
-  { id: 10, title: 'ALKALINE TRIO', price: 200000, qty: 1, image: 'img4.jpg' }
+  { id: 10, title: 'TS - ALKALINE TRIO', price: 200000, qty: 1, image: 'img4.jpg' }
 ];
 
 function formatNumber(n, c, d, t) {
