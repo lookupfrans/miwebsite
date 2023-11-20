35cac27f42d49c905a9389d55118733c1d40f69e
Fransen Steven Roby <99979348+lookupfrans@users.noreply.github.com>
2023-11-20 13:15:29 +0700

    Update ua.php

--- ua.php
+++ ua.php
@@ -11,6 +11,7 @@ include('header.php');
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Stevenhoven Store</title>
   <link rel="stylesheet" href="responsive.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <link rel="icon" type="image/png" sizes="180x180" href="favo.png">
   <style>
     .heading {
