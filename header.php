fa6ee4817fcaf9f2e660073acb4e26ef9d930656
Fransen Steven Roby <99979348+lookupfrans@users.noreply.github.com>
2023-11-21 07:18:30 +0700

    Update header.php

--- header.php
+++ header.php
@@ -88,7 +88,7 @@
 
 <body>
     <div class="loader" style="text-align:center;height:100vh; width:100vw;overflow:hidden;background:#000;">
-    <img class="image-title" alt="" src="mylogo.jpg" style="width: 20%; height:20%;position:absolute;"/>
+    <img class="image-title" alt="" src="mylogo.png" style="width: 20%; height:20%;position:absolute;"/>
     <div></div>
   </div>
   <div class="content">
