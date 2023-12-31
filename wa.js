39be5a402407ed44314bed1117bcf86315e79d24
Fransen Steven Roby <99979348+lookupfrans@users.noreply.github.com>
2023-12-31 22:46:40 +0700

    Update wa.js

--- wa.js
+++ wa.js
@@ -1,5 +1,5 @@
 document.getElementById("whatsapp-button").addEventListener("click", function() {
-    var phoneNumber = "6287798961630";
+    var phoneNumber = "6282161907513";
     var url = "https://wa.me/" + phoneNumber;
     window.open(url, "_blank");
 });
