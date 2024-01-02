0b2a76bf7a726338c28db4811b60e2e4050512c8
Fransen Steven Roby <99979348+lookupfrans@users.noreply.github.com>
2024-01-02 14:03:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -149,15 +149,15 @@ i {
               </tr>
               <tr>
                 <td>Kode Item : </td>
-                <td> <input size="70" type="text" name="alamat2" autocomplete="on"><br>&nbsp;&nbsp;&nbsp;&nbsp;(<i class="paragraph">dipisah dengan notasi koma berurutan apabila lebih dari satu</i>)</td>
+                <td> <input size="70" type="text" name="alamat2" autocomplete="on"><br>&nbsp;&nbsp;&nbsp;&nbsp;<i class="paragraph">dipisah dengan notasi koma berurutan apabila lebih dari satu</i></td>
               </tr>
               <tr>
                 <td>Kuantitas : </td>
-                <td> <input size="70" type="text" name="alamat2" autocomplete="on"><br>&nbsp;&nbsp;&nbsp;&nbsp;(<i class="paragraph">dipisah dengan notasi koma berurutan apabila lebih dari satu</i>)</td>
+                <td> <input size="70" type="text" name="alamat2" autocomplete="on"><br>&nbsp;&nbsp;&nbsp;&nbsp;<i class="paragraph">dipisah dengan notasi koma berurutan apabila lebih dari satu</i></td>
               </tr>
               <tr>
                 <td>Ukuran: </td>
-                <td> <input size="70" type="text" name="alamat2" autocomplete="on"><br>&nbsp;&nbsp;&nbsp;&nbsp;(<i class="paragraph">dipisah dengan notasi koma berurutan apabila lebih dari satu</i>)</td>
+                <td> <input size="70" type="text" name="alamat2" autocomplete="on"><br>&nbsp;&nbsp;&nbsp;&nbsp;<i class="paragraph">dipisah dengan notasi koma berurutan apabila lebih dari satu</i></td>
               </tr>
               <tr>
                 <td>Kota: </td>
