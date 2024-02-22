c4fa3b514734aed5b86d5e2a259618f625a8bf53
Fransen Steven Roby <99979348+lookupfrans@users.noreply.github.com>
2024-02-22 10:10:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -149,7 +149,8 @@ i {
               </tr>
               <tr>
                 <td>Kode Item : </td>
-                <td> <input size="70" type="text" name="alamat2" autocomplete="on"><br>&nbsp;&nbsp;&nbsp;&nbsp;<i class="paragraph">*dipisah dengan notasi koma berurutan apabila lebih dari satu</i></td>
+                <td> <input size="70" type="text" name="alamat2" autocomplete="on"><br>&nbsp;&nbsp;&nbsp;&nbsp;<i class="paragraph">*dipisah dengan notasi koma berurutan apabila lebih dari satu</i>
+                <i>ex: 01,02,05</i></td>
               </tr>
               <tr>
                 <td>Kuantitas : </td>
